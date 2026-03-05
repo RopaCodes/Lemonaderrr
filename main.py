71f60eaa669c6ac67ab24b188ce98e69a41a5bfe
RopaCodes <ropacodezz@gmail.com>
2026-03-05 17:57:42 +1300

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,10 @@ class Game:
         self.display = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.clock =  pygame.time.Clock()
         pygame.display.set_caption('Lemonaderrr')
-
+        self.drink_progress = 0
         self.frame_num = 0
 
         #OBJECT CREATION
-        self.basic_lem_menu = BasicLemMenu(self.display, self.screen_width, self.screen_height)
         self.basic_lem_menu = BasicLemMenu(self.display, self.screen_width, self.screen_height)
         self.money_status = MoneySystem(self.display,self.screen_width,self.screen_height,0) 
         
@@ -28,9 +27,9 @@ class Game:
         self.lemonade_stand = LemonadeStand(self.display, self.screen_width, self.screen_height)
         self.extras_container = ExtrasContainer(self.display, self.screen_width, self.screen_height)
         self.fruit_container = FruitContainer(self.display, self.screen_width, self.screen_height)
-        self.ice_bucket = IceBucket(self.display, self.screen_width, self.screen_height)
-        self.sugar_bag = Sugar(self.display, self.screen_width, self.screen_height)
-        self.water_jug = WaterJug(self.display, self.screen_width, self.screen_height,self.basic_lem_menu,self.frame_num,self.money_status)
+        self.ice_bucket = IceBucket(self.display, self.screen_width, self.screen_height,self.basic_lem_menu,self.money_status,self)
+        self.sugar_bag = Sugar(self.display, self.screen_width, self.screen_height,self.basic_lem_menu,self.money_status,self)
+        self.water_jug = WaterJug(self.display, self.screen_width, self.screen_height,self.basic_lem_menu,self.money_status,self)
         
 
         #SPRITE SHEET
@@ -38,6 +37,14 @@ class Game:
         self.sprite_sheet_basic = SpriteSheet(self.sprite_sheet_basic_lem)
         self.frame0 = self.sprite_sheet_basic.get_img(self.frame_num,256,256,1)
         self.scaled_drink = pygame.transform.scale(self.frame0,(110,110))
+    
+    def check_recipie_complete(self):
+        if(
+            self.water_jug.correct_amm and
+            self.ice_bucket.correct_amm and
+            self.sugar_bag.correct_amm
+        ):
+            self.money_status.money_earned += 5
 
     def run(self):
         while True:
@@ -55,7 +62,12 @@ class Game:
                         sys.exit()
 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
+                    #mouse_pos = event.pos
                     self.water_jug.check_collision()
+                    self.sugar_bag.check_collision()
+                    self.ice_bucket.check_collision()
+
+                    self.check_recipie_complete()
 
             #update display
             self.update_display()
@@ -73,7 +85,7 @@ class Game:
         self.sugar_bag.draw_img()
         self.water_jug.draw_img()   
         #self.display.blit(self.scaled_drink,(540,(self.screen_height-354)))
-        current_frame = self.water_jug.frame_number
+        current_frame = self.drink_progress
         frame_img = self.sprite_sheet_basic.get_img(current_frame, 256, 256, 1)
         scaled = pygame.transform.scale(frame_img, (110,110))
         self.display.blit(scaled,(540,(self.screen_height-354)))
